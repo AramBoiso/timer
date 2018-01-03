5b0cf7993c177c9bb330d60bc128fcbdee448601
Aram <boiso.ska8@gmail.com>
2018-01-03 17:36:31 -0600

    componente timer creado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string;
+  minutos:number;
+  segundos:number;
+  isPaused:boolean;
+  buttonLabel:string;
 
     constructor(){
-      this.title = "Hola Mundo";
+      this.resetTimer();
+      setInterval(()=> this.tick(), 1000);
     }
-    
+
+    resetTimer():void{ // Primera funcion que se ejecuta
+      this.minutos = 24;
+      this.segundos = 59;
+      this.buttonLabel = "  Empezar";
+      this.togglePause();
+    }
+
+    tick():void{ //Segunda funcion que se ejecuta
+      if(!this.isPaused){ 
+          this.buttonLabel = "Detener";
+  
+      if(--this.segundos < 0){
+        this.segundos = 59;
+        if(--this.minutos < 0){
+          this.minutos = 24;
+          this.segundos = 59;
+        }
+      } 
+    } 
+     
+    }
+
+    togglePause():void{ // boton de pausa
+        
+        this.isPaused = !this.isPaused;  // = true
+      
+        if(this.minutos < 24 || this.segundos < 59){
+          this.buttonLabel = this.isPaused ? "Reaunudar" : "Detener";
+        }
+
+    } 
+
 }
